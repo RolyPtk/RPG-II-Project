03769c11b1d9b625c3bdbcfec372420dbc9ad03c
Rares793 <criticaltime007@gmail.com>
2026-05-06 23:35:10 +0300

    Added modifications to scene so AI can pathfind and tried to apply the player's attack method to the Enemy

--- GameName/Assets/Scripts/Enemy/Enemy.cs
+++ GameName/Assets/Scripts/Enemy/Enemy.cs
@@ -1,9 +1,60 @@
 using UnityEngine;
+using Pathfinding;
 
 public class Enemy : Entity
 {
-    // have fun
-    // here it would be a nice ideea to add things only the enemy would need, we can delete this later if need be
-    // we just need a script of attacking and decision making besides movement, like to attack which you can do by accessing the 
-    // reachedEndOfPath variable of EnemyAI I think 
+    [Header("Attack")]
+    public int attackDamage = 20;
+    public float attackCooldown = 1f;
+    public float attackRadius = 4.5f;
+
+    private float lastAttackTime;
+    private Transform player;
+    private AIPath aiPath;
+
+    protected override void Start()
+    {
+        base.Start();
+        player = GameObject.FindWithTag("Player").transform;
+        aiPath = GetComponent<AIPath>();
+
+        if (player == null)
+        Debug.LogError("Enemy cannot find Player! Is the tag set correctly?");
+    if (aiPath == null)
+        Debug.LogError("Enemy has no AIPath component!");
+    }
+
+    void Update()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (distanceToPlayer <= attackRadius)
+        {
+            aiPath.canMove = false; 
+
+            if (Time.time >= lastAttackTime + attackCooldown)
+            {
+                lastAttackTime = Time.time;
+                var entityComponent = player.GetComponent<Entity>();
+                Debug.Log("Entity component found: " + (entityComponent != null));
+                player.GetComponent<Entity>()?.TakeDamage(attackDamage);
+                Debug.Log("Enemy attacked player for " + attackDamage + " damage");
+            }
+        }
+        else
+        {
+            aiPath.canMove = true;
+        }
+    }
+
+    protected override void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRadius);
+    }
 }
--- GameName/Assets/Scripts/Entity.cs
+++ GameName/Assets/Scripts/Entity.cs
@@ -17,6 +17,6 @@ public abstract class Entity : MonoBehaviour
         Destroy(gameObject);
     }
         
-    protected int maxHealth = 100;
+    public int maxHealth = 100;
     protected int currentHealth;
 }
