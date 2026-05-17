135c09ff9845c11b6825aacf9557b7174a198ad9
RolyPtk <roland.dudas01@gmail.com>
2026-05-17 18:05:28 +0300

    New map design + altele
    Am facut o harta noua, cu mai multe layere si cladiri. Am pus animatii cu collidere la cladire (player sta in spatele cladirii = pereti si acoperis transparent, la fel si peretului cand te afli inauntru). De acum playerul poate merge si in spatele unor copaci. Am facut niste chesturi cu care playerul poate interactiona apasand tasta E (apare si mesaj cand se afla in zona). S-a incercat modelul tip Zelda pt harta (cat de mult posibil). S-a refacut tranzitia intre nivelul 1 si 2 partial. Am dat resize la collider box pt player (la ultimul merge a devenit imens).

--- GameName/Assets/Scripts/MapManager/SceneController.cs
+++ GameName/Assets/Scripts/MapManager/SceneController.cs
@@ -1,11 +1,15 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneController : MonoBehaviour
 {
     public static SceneController Instance;
+    public Image fadeImage;
+    public float fadeDuration = 1f;
 
-    public void Awake()
+    void Awake()
     {
         if (Instance == null)
         {
@@ -20,12 +24,55 @@ public class SceneController : MonoBehaviour
 
     public void NextLevel()
     {
-        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
-        
+        int next = SceneManager.GetActiveScene().buildIndex + 1;
+        if (next < SceneManager.sceneCountInBuildSettings)
+            StartCoroutine(TransitionToScene(next));
+        else
+            Debug.Log("Ai terminat jocul!");
     }
 
     public void LoadScene(string sceneName)
     {
-                SceneManager.LoadSceneAsync(sceneName);
+        StartCoroutine(TransitionToSceneByName(sceneName));
+    }
+
+    private IEnumerator TransitionToScene(int sceneIndex)
+    {
+        yield return StartCoroutine(Fade(0f, 1f));
+        AsyncOperation load = SceneManager.LoadSceneAsync(sceneIndex);
+        load.allowSceneActivation = false;
+        while (load.progress < 0.9f)
+            yield return null;
+        load.allowSceneActivation = true;
+        yield return new WaitForEndOfFrame();
+        yield return StartCoroutine(Fade(1f, 0f));
+    }
+
+    private IEnumerator TransitionToSceneByName(string sceneName)
+    {
+        yield return StartCoroutine(Fade(0f, 1f));
+        AsyncOperation load = SceneManager.LoadSceneAsync(sceneName);
+        load.allowSceneActivation = false;
+        while (load.progress < 0.9f)
+            yield return null;
+        load.allowSceneActivation = true;
+        yield return new WaitForEndOfFrame();
+        yield return StartCoroutine(Fade(1f, 0f));
+    }
+
+    private IEnumerator Fade(float from, float to)
+    {
+        if (fadeImage == null) yield break;
+        float elapsed = 0f;
+        Color c = fadeImage.color;
+        while (elapsed < fadeDuration)
+        {
+            elapsed += Time.deltaTime;
+            c.a = Mathf.Lerp(from, to, elapsed / fadeDuration);
+            fadeImage.color = c;
+            yield return null;
+        }
+        c.a = to;
+        fadeImage.color = c;
     }
 }
